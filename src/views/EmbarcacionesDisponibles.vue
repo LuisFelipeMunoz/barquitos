c97a0e13d4024c6b1120474cd9f499df07f5ccc5
isidoraaps <75916730+isidoraaps@users.noreply.github.com>
2021-01-06 22:23:18 -0300

    feat: agregue la encuesta

--- src/views/EmbarcacionesDisponibles.vue
+++ src/views/EmbarcacionesDisponibles.vue
@@ -19,6 +19,88 @@
         @click-descargar="descargarComprobante"
       ></DescargarComprobanteArriendo>
     </v-dialog>
+
+    <!------------------------------------------------------->
+    <!-- MENU -->
+    <!------------------------------------------------------->
+    
+    
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn block elevation="2"
+          color="success"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          ENCUESTA
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+          <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Responder
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="headline grey lighten-2">
+          ENCUESTA
+        </v-card-title>
+
+        <v-card-text>
+          CALIFICA DE 1 A 5 NUESTRO SERVICIO :
+          <v-rating
+            v-model="rating"
+            background-color="indigo lighten-3"
+            color="indigo"
+            size="64"
+          ></v-rating>
+        </v-card-text>
+        <v-card-text>
+          HAZ UN COMENTARIO RESPECTO A TU EXPERENCIA :
+          <v-container fluid>
+            <v-textarea
+              autocomplete="email"
+              label="Ingresalo aqui"
+            ></v-textarea>
+          </v-container>
+        </v-card-text>
+        
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+            Enviar Encuesta
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    
+
+ 
   </v-container>
 </template>
 
@@ -42,6 +124,12 @@ export default class EmbarcacionesDisponibles extends Vue {
   dialogoComprobante = false;
 
   barcos = ["barco", "barco", "barco", "barco"];
+  items= [
+        { title: 'ENCUESTA 1' },
+        { title: 'ENCUESTA 2' },
+        { title: 'ENCUESTA 3' },
+        
+      ];
 
   abrirDialogoArrendar() {
     this.dialogoArrendar = true;
@@ -55,5 +143,6 @@ export default class EmbarcacionesDisponibles extends Vue {
   descargarComprobante() {
     this.dialogoComprobante = false;
   }
+
 }
 </script>

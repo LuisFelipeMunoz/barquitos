f60ed63c4eaf3a309e1769ec296792806455bf7e
LuisFelipeMunoz <luis.munoz@alu.ucm.cl>
2021-01-08 00:41:03 -0300

    feat: agregados campos faltantes formulario pago y seguro

--- src/components/pagos/Formulario.vue
+++ src/components/pagos/Formulario.vue
@@ -9,35 +9,49 @@
     <v-col cols="12" class="px-3">
       <v-text-field
         v-model="valor"
-        label="Valor"
+        label="valor"
         hide-details="auto"
         :error-messages="valorError"
         @input="$v.valor.$touch()"
         @blur="$v.valor.$touch()"
       ></v-text-field>
     </v-col>
-    <v-col cols="12" class="px-3 mb-3">
+    <v-col cols="12" class="px-3">
       <v-select
         v-model="tipo"
-        label="Tipo"
+        label="tipo"
         hide-details="auto"
+        :items="tipos"
         :error-messages="tipoError"
         @input="$v.tipo.$touch()"
         @blur="$v.tipo.$touch()"
       ></v-select>
     </v-col>
-
+    <v-col cols="12" class="px-3">
+      <v-select
+        v-model="arriendo"
+        label="arriendo"
+        hide-details="auto"
+        :items="arriendos"
+        return-object
+        :error-messages="arriendoError"
+        @input="$v.arriendo.$touch()"
+        @blur="$v.arriendo.$touch()"
+      ></v-select>
+    </v-col>
     <v-col cols="12">
-      <v-btn color="success" large block elevation="2" @click="guardar"
-        >guardar</v-btn
-      >
+      <v-btn color="success" large block elevation="2" @click="guardar">
+        guardar
+      </v-btn>
     </v-col>
   </v-row>
 </template>
 
 <script lang="ts">
 // decoradores
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue } from "vue-property-decorator";
+// tipos
+import { Arriendo } from "@/typings/store";
 //vuelidate
 import { required } from "vuelidate/lib/validators";
 
@@ -45,12 +59,17 @@ import { required } from "vuelidate/lib/validators";
   validations: {
     valor: { required },
     tipo: { required },
+    arriendo: { required },
   },
 })
 export default class FormularioUsuario extends Vue {
+  @Prop() readonly arriendos!: Array<Arriendo>;
+
   valor = "";
-  tipo = 0;
-  patente = "";
+  tipo = "";
+  arriendo: Arriendo | null | undefined = null;
+
+  tipos = ["debito", "credito"];
 
   get valorError() {
     const errors: Array<string> = [];
@@ -63,6 +82,13 @@ export default class FormularioUsuario extends Vue {
     const errors: Array<string> = [];
     if (!this.$v.tipo.$dirty) return errors;
     if (!this.$v.tipo.required) errors.push("Requerido");
+    return errors;
+  }
+
+  get arriendoError() {
+    const errors: Array<string> = [];
+    if (!this.$v.arriendo.$dirty) return errors;
+    if (!this.$v.arriendo.required) errors.push("Requerido");
     return errors;
   }
 

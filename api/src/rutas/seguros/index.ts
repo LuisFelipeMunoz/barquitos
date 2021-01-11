732cf98573e7ef640980e87281b4b8caf226b208
caritoislove <caritohyom@gmail.com>
2021-01-11 02:59:17 -0300

    feat: rutas pagos, seguros, encuestas

--- api/src/rutas/seguros/index.ts
+++ api/src/rutas/seguros/index.ts
@@ -1,35 +1,49 @@
-app.get("/api/seguros", async function(req, res) {
-  let resultado: Resultado = {};
-  try {
-    connection = await db.getConnection();
+import { Express } from "express";
+// conexion oracle
+import * as db from "../../db";
+// metodos tabla usuario
+import { lista } from "../../tablas/seguros";
+// tipos
+import { Resultado, EntradaBD } from "../../typings/api";
 
-    const rawBD = await listaSeguros(connection);
-    rawBD.rows?.forEach((item) => {
-      const datos = item as Array<any>;
+let connection: db.Connection | undefined = undefined;
 
-      const seguro: EntraBD = {};
+const seguros = (app: Express) => {
+  app.get("/api/seguros", async function(req, res) {
+    let resultado: Resultado = {};
+    try {
+      connection = await db.getConnection();
 
-      datos.forEach((val, index) => {
-        const nombreCampo = rawBD.metaData
-          ? rawBD.metaData[index].name
-          : "COL" + index.toString();
-        seguro[nombreCampo] = val;
+      const rawBD = await lista(connection);
+      rawBD.rows?.forEach((item) => {
+        const datos = item as Array<any>;
+
+        const seguro: EntradaBD = {};
+
+        datos.forEach((val, index) => {
+          const nombreCampo = rawBD.metaData
+            ? rawBD.metaData[index].name
+            : "COL" + index.toString();
+          seguro[nombreCampo] = val;
+        });
+
+        resultado[seguro.ID_SEGURO] = seguro;
       });
-
-      resultado[seguro.ID_SEGURO] = seguro;
-    });
-  } catch (err) {
-    console.error(err);
-    resultado[0] = { error: err };
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.error(err);
-        resultado[0] = { error: err };
+    } catch (err) {
+      console.error(err);
+      resultado[0] = { error: err };
+    } finally {
+      if (connection) {
+        try {
+          await connection.close();
+        } catch (err) {
+          console.error(err);
+          resultado[0] = { error: err };
+        }
       }
     }
-  }
-  res.send(resultado);
-});
+    res.send(resultado);
+  });
+};
+
+export default seguros;
